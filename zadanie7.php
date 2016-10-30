4764d303ee28f63e4408af5cfb42b05189b39daf
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-30 15:21:31 +1100

    #7[1] Задача 7, по работе с простым регулярным выражением.

---
+++ zadanie7.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(-1);
+mb_internal_encoding('utf-8');
+
+$text = 'Карл у Клары украл кораллы';
+$regexp= '/К/u';
+$result=preg_replace($regexp, ' ', $text);
+echo $result;
+echo "<br>";
+
+$othertext = 'Две бутылки лимонада';
+$regexp= '/Две/u';
+$result=preg_replace($regexp, 'Три', $othertext);
+echo $result;
