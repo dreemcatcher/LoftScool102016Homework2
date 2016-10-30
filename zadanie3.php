58a7fc99f2a06a1ffce728308267b7b4c62996af
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-30 14:31:00 +1100

    #3[1] Задача 3, по выполнению арифметических действий при помощи функций принимающей неизвестное число аргументов.

---
+++ zadanie3.php
@@ -0,0 +1,54 @@
+<?php
+
+function  calcEverything()
+{
+    $numbers=0;
+    $char=func_get_arg(0);
+    echo '<pre>';
+    print_r($char);  // штуковина для тестирования массива.
+    echo '</pre>';
+    echo "Получено действие $char .<br>";
+    if($char=='+'){
+        for ($i=1; $i<func_num_args(); $i++)  // Первый элемент это действие.
+        {
+            echo "Параметр номер $i: " . func_get_arg($i) . "<br>";
+            $numbers=$numbers+func_get_arg($i);
+        }
+        return $numbers;
+    }elseif($char=='-'){
+        $numbers=func_get_arg(1);            // Из первого элемента мы вычитаем все остальные
+        for ($i=2; $i<func_num_args(); $i++) // Поэтому тут отсчёт со второго
+        {
+            echo "Параметр номер $i: " . func_get_arg($i) . "<br>";
+            $numbers=$numbers-func_get_arg(1);
+        }
+        return $numbers;
+    }elseif($char=='/'){
+        $numbers=func_get_arg(1);
+        for ($i=2; $i<func_num_args(); $i++)
+        {
+            $x=func_get_arg($i);
+            echo "Параметр номер $i: " . func_get_arg($i) . "<br>";
+            //echo "Выполняем деление".$numbers." / " .$x;
+            $numbers=$numbers/$x; // Делится на непосредственно взятую переменную из массива не хочет, присваиваем её переменной и делим.
+            //echo " = ".$numbers."<br>";
+        }
+        return $numbers;
+    }elseif($char=='*'){
+        $numbers=func_get_arg(1);
+        for ($i=2; $i<func_num_args(); $i++)
+        {
+            $x=func_get_arg($i);
+            echo "Параметр номер $i: " . func_get_arg($i) . "<br>";
+            $numbers=$numbers*$x;
+        }
+        return $numbers;
+    }else{
+        echo "Неизвестное действие.<br>";
+    }
+}
+
+echo  calcEverything('+', 1, 2, 3, 5.2);
+echo  calcEverything('-', 1, 2, 3, 5.2);
+echo  calcEverything('/', 1, 2, 3, 5.2);
+echo  calcEverything('*', 1, 2, 3, 5.2);
